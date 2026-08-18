a357304e06554e96762457f2f8f2b63c710c1009
bgallegov <b.gallegov@lanuevaamerica.edu.co>
2026-08-17 23:16:37 -0500

    App.jsx

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="home">
+      <h1>Paradise Nursery</h1>
+      <p>Encuentra las mejores plantas para tu hogar</p>
+
+      <button>Comenzar</button>
+    </div>
+  );
+}
+
+export default App;
